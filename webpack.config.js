006a8dfbf2b810fa7c1764e887b9c403face9253
Nick Konstantinov <nick.konstantinov.job@gmail.com>
2023-06-29 12:23:54 +0400

    feat: add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,39 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+  target: 'web',
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
   entry: './index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
   },
   devServer: {
-    port: 4200,
+    static: {
+      directory: path.join(__dirname, './dist'),
+    },
+    hot: true,
+    liveReload: true,
+    open: true,
+    watchFiles: ['.src/index.html'],
   },
   plugins: [
     new HTMLWebpackPlugin({
       template: './index.html',
     }),
     new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+      ],
+    }),
   ],
   module: {
     rules: [
